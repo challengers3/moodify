ef4aa7c16fb5df433b360c4e4faba1a47dd9afa2
Phongtlam <Phongtlam@gmail.com>
2017-05-20 16:58:49 -0700

    Working state

--- react-client/src/components/Main.jsx
+++ react-client/src/components/Main.jsx
@@ -66,27 +66,6 @@ class Main extends React.Component {
       };
       annyang.addCommands(commands);
       annyang.debug();
-
-      window.fbAsyncInit = () => {
-        FB.init({
-          appId: Facebook_key,
-          cookie: true,
-          xfbml: true,
-          version: 'v2.1',
-        });
-        FB.getLoginStatus((response) => {
-          this.statusChangeCallback(response);
-        });
-      };
-
-      (function (d, s, id) {
-        let js,
-          fjs = d.getElementsByTagName(s)[0];
-        if (d.getElementById(id)) return;
-        js = d.createElement(s); js.id = id;
-        js.src = '//connect.facebook.net/en_US/sdk.js';
-        fjs.parentNode.insertBefore(js, fjs);
-      }(document, 'script', 'facebook-jssdk'));      
     }
   }
 
